26040cd34ff2f0f425c835fc1022058361bf1e60
marco <dobermannpoker86@gmail.com>
2020-03-19 18:04:39 +0100

    tutto

--- train.js
+++ train.js
@@ -9,7 +9,6 @@ var prezzoBiglietto = kmDaPercorrere * 0.21;
 
 // Il prezzo del biglietto è definito in base ai km (0.21 € al km)
 
-
 //fascia 20% da 0 a 17       <18
 var scontoFascia20 = (prezzoBiglietto * 20)/100;
 
@@ -17,16 +16,27 @@ var scontoFascia20 = (prezzoBiglietto * 20)/100;
 //fascia 40% da 66 a 100     >65
 var scontoFascia40 = (prezzoBiglietto * 40)/100;
 
+var prezzoBigliettoFinale  = "io non ho valore";
+
 //se l'età è inferiore < 18
  if (eta < 18) {
 
      //applichiamo lo sconto di fascia 20
      var prezzoScontato = prezzoBiglietto - scontoFascia20;
 
+     prezzoBigliettoFinale = prezzoScontato;
+
 }  else if (eta> 65) {
 
     //applicahiamo lo sconto di fascia 40
     var prezzoScontato = prezzoBiglietto - scontoFascia40;
+
+    prezzoBigliettoFinale = prezzoScontato;
+
+} else {
+
+    // altrimenti il prezzo finale del biglietto è quello di base
+    prezzoBigliettoFinale = prezzoBiglietto;
 }
 
-document.getElementById("generator").innerHTML = prezzoScontato;
+document.getElementById("generator").innerHTML = prezzoBigliettoFinale;
